91feb4c0aa5c30c5b533c427e3eccf17f8f5a31d
Antonio Melo <161260848+AntonioMeloDeveloper@users.noreply.github.com>
2024-03-09 17:20:52 -0300

    aula 01

--- php/exercícios/loops.php
+++ php/exercícios/loops.php
@@ -4,7 +4,7 @@ $i=0;
 $tam=5;
 
 for($i=0; $i < $tam; $i++ ){
-    echo "Valor de i: $i <br/>"
+    echo "Valor de i: $i <br/>";
 }
 
 // 01 variável de controle, 02 condição de parada do for, agora é incrementar a variável $i de um e um
